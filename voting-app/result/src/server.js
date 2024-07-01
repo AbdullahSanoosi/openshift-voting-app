36aa164bde74733412c50fb29567fc793a1c3dc7
Abdullah Sanoosi <124656977+AbdullahSanoosi@users.noreply.github.com>
2024-07-01 17:09:26 +0530

    Commenting the whole file.
    Commenting the whole file to add only the code to enable debugging for Node.js application.

--- voting-app/result/src/server.js
+++ voting-app/result/src/server.js
@@ -1,103 +1,125 @@
-var express = require('express'),
-    async = require('async'),
-    pg = require('pg'),
-    { Pool } = require('pg'),
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+// var express = require('express'),
+//     async = require('async'),
+//     pg = require('pg'),
+//     { Pool } = require('pg'),
+//     path = require('path'),
+//     cookieParser = require('cookie-parser'),
+//     bodyParser = require('body-parser'),
+//     methodOverride = require('method-override'),
+//     app = express(),
+//     server = require('http').Server(app),
+//     io = require('socket.io')(server);
 
-io.set('transports', ['polling']);
+// io.set('transports', ['polling']);
 
-var port = process.env.PORT || 8080;
+// var port = process.env.PORT || 8080;
 
-io.sockets.on('connection', function (socket) {
+// io.sockets.on('connection', function (socket) {
 
-  socket.emit('message', { text : 'Welcome!' });
+//   socket.emit('message', { text : 'Welcome!' });
 
-  socket.on('subscribe', function (data) {
-    socket.join(data.channel);
-  });
-});
+//   socket.on('subscribe', function (data) {
+//     socket.join(data.channel);
+//   });
+// });
 
-var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
-});
+// var pool = new pg.Pool({
+//   connectionString: 'postgres://postgres:postgres@db/postgres'
+// });
 
-async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
-      if (err) {
-        console.error("Waiting for db");
-      }
-      callback(err, client);
-    });
-  },
-  function(err, client) {
-    if (err) {
-      return console.error("Giving up");
-    }
-    console.log("Connected to db");
-    getVotes(client);
-  }
-);
+// async.retry(
+//   {times: 1000, interval: 1000},
+//   function(callback) {
+//     pool.connect(function(err, client, done) {
+//       if (err) {
+//         console.error("Waiting for db");
+//       }
+//       callback(err, client);
+//     });
+//   },
+//   function(err, client) {
+//     if (err) {
+//       return console.error("Giving up");
+//     }
+//     console.log("Connected to db");
+//     getVotes(client);
+//   }
+// );
 
-function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
+// function getVotes(client) {
+//   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
+//     if (err) {
+//       console.error("Error performing query: " + err);
+//     } else {
+//       var votes = collectVotesFromResult(result);
+//       io.sockets.emit("scores", JSON.stringify(votes));
+//     }
 
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
-}
+//     setTimeout(function() {getVotes(client) }, 1000);
+//   });
+// }
 
-function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+// function collectVotesFromResult(result) {
+//   var votes = {a: 0, b: 0};
 
-  result.rows.forEach(function (row) {
-    votes[row.vote] = parseInt(row.count);
-  });
+//   result.rows.forEach(function (row) {
+//     votes[row.vote] = parseInt(row.count);
+//   });
 
-  return votes;
-}
+//   return votes;
+// }
 
-app.use(cookieParser());
-app.use(bodyParser());
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-  next();
-});
+// app.use(cookieParser());
+// app.use(bodyParser());
+// app.use(methodOverride('X-HTTP-Method-Override'));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+//   next();
+// });
 
-app.use(express.static(__dirname + '/views'));
+// app.use(express.static(__dirname + '/views'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.resolve(__dirname + '/views/index.html'));
+// });
 
-server.listen(port, function () {
-  var port = server.address().port;
-  console.log('App running on port ' + port);
+// server.listen(port, function () {
+//   var port = server.address().port;
+//   console.log('App running on port ' + port);
+// });
+
+// app.get('/test-db', async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query('SELECT 1');
+//     client.release();
+//     res.send(`DB Connection Test: ${result.rows[0]['?column?']}`);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Database connection failed');
+//   }
+// });
+
+
+const pg = require('pg');
+
+const pool = new pg.Pool({
+  connectionString: 'postgres://postgres:postgres@db:5432/postgres'
 });
 
 app.get('/test-db', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT 1');
+    res.send('Database connected successfully!');
     client.release();
-    res.send(`DB Connection Test: ${result.rows[0]['?column?']}`);
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Database connection failed');
+    console.error('Database connection failed:', err); // Log the detailed error
+    res.status(500).send(`Database connection failed: ${err.message}`);
   }
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
